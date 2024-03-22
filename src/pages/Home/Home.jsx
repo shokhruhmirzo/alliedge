3d769aacefad85f4b865626e5ecb149b99a39536
shokhruhmirzo <shaalimov923@gmail.com>
2024-03-22 20:05:05 +0500

    add some things

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import Hero from '../../components/Hero/Hero'
 import Extrience from '../../components/Extrience/Extrience'
-import About from '../Aboutpage/Aboutpage'
 import Group from '../../components/Group/Group'
+import About from '../../components/About/About'
 
 function Home() {
   return (
